035bee1dd766f2fb6fe0f280aa2222b7d20ea59a
Shivam Rathod <shivamrathod145@gmail.com>
2026-01-23 00:10:01 +0530

    Lazy Loading

--- src/components/DressCard.tsx
+++ src/components/DressCard.tsx
@@ -52,6 +52,7 @@ const DressCard = ({ dress, onViewDetails }: DressCardProps) => {
           <img
             src={primaryImage}
             alt={dress.name}
+            loading="lazy"
             className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-105"
           />
         )}
